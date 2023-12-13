8c7886d16aafc9804dd3d672de930699746e70b3
Marcio <mwm.junior@hotmail.com>
2023-12-13 09:54:33 -0300

    version 1.0

---
+++ Event+/webapi.event+/Controllers/ComentariosEventoController.cs
@@ -0,0 +1,156 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.CognitiveServices.ContentModerator;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using webapi.event_.Domains;
+using webapi.event_.Repositories;
+
+namespace webapi.event_.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class ComentariosEventoController : ControllerBase
+    {
+        ComentariosEventoRepository comentario = new ComentariosEventoRepository();
+
+        // armazena dados do servico da api externa (IA - Azure)
+        private readonly ContentModeratorClient _contentModeratorClient;
+
+        /// <summary>
+        /// Construtor que recebe os dados necessarios para acesso sao servico externo 
+        /// </summary>
+        /// <param name="contentModeratorClient">objeto do tipo ContentModeratorClient</param>
+        public ComentariosEventoController(ContentModeratorClient contentModeratorClient)
+        {
+            _contentModeratorClient= contentModeratorClient;
+        }
+
+
+        [HttpPost("ComentarioIA")]
+
+        public async Task <IActionResult> PostIa(ComentariosEvento novocomentario)
+
+        {
+            try
+            {
+                //if(comentario.Descricao !=null) || comentario.Descricao =="")
+                //if (string.IsNullOrEmpty(comentario.Descricao))
+                //if((comentario.Descricao) .IsNullOrEmpty)
+
+                if(string.IsNullOrEmpty(novocomentario.Descricao))
+                {
+                    return BadRequest("A descricao do comenterio nao pode estar vazio ou nulo!");
+                }
+
+                using var stream = new MemoryStream(Encoding.UTF8.GetBytes(novocomentario.Descricao));
+
+                var moderationResult = await _contentModeratorClient.TextModeration.ScreenTextAsync("text/plain", stream, "por", false,false, null, true);
+                
+                 if (moderationResult.Terms != null) 
+                {
+                    novocomentario.Exibe = false;
+
+                    comentario.Cadastrar(novocomentario);
+                 
+                }
+
+                 else
+                {
+
+                    novocomentario.Exibe = true;
+                    comentario.Cadastrar(novocomentario);
+                }
+                 return StatusCode(201,novocomentario);
+
+            }
+            catch (Exception error)
+            {
+
+                return BadRequest(error.Message);    
+            }
+        }
+
+
+        [HttpGet("ListarSomenteExibe")]
+        public IActionResult GetShow()
+        {
+            try
+            {
+                return Ok(comentario.ListarSomenteExibe());
+            }
+
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(comentario.Listar());
+            }
+
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("BuscarPorIdUsuario")]
+        public IActionResult GetByIdUser(Guid idUsuario, Guid idEvento)
+        {
+            try
+            {
+                return Ok(comentario.BuscarPorIdUsuario(idUsuario,idEvento));
+            }
+
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPost]
+
+        public IActionResult Post(ComentariosEvento novoComentario)
+        {
+            try
+            {
+                comentario.Cadastrar(novoComentario);
+
+                return StatusCode(201, novoComentario);
+            }
+
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+                throw;
+            }
+        }
+
+        [HttpDelete("{id}")]
+             public IActionResult Delete(Guid id)
+        {
+            try
+            {
+                comentario.Deletar(id);
+
+                return StatusCode(204);
+            }
+
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+                throw;
+            }
+        }
+
+
+
+    }
+}
---
+++ Event+/webapi.event+/Repositories/EventoRepository.cs
@@ -0,0 +1,135 @@
+﻿using System.ComponentModel.Design;
+using webapi.event_.Contexts;
+using webapi.event_.Domains;
+using webapi.event_.Interfaces;
+
+namespace webapi.event_.Repositories
+{
+    public class EventoRepository : IEventoRepository
+    {
+        private readonly Event_Context _context;
+
+        public EventoRepository()
+        {
+            _context = new Event_Context();
+        }
+
+        public void Atualizar(Guid id, Evento evento)
+        {
+            try
+            {
+                Evento eventoBuscado = _context.Evento.Find(id)!;
+
+                if (eventoBuscado != null)
+                {
+                    eventoBuscado.DataEvento = evento.DataEvento;
+                    eventoBuscado.NomeEvento = evento.NomeEvento;
+                    eventoBuscado.Descricao = evento.Descricao;
+                    eventoBuscado.IdTipoEvento = evento.IdTipoEvento;
+                }
+
+                _context.Evento.Update(eventoBuscado!);
+
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public Evento BuscarPorId(Guid id)
+        {
+            try
+            {
+                return _context.Evento.Find(id)!;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public void Cadastrar(Evento evento)
+        {
+            try
+            {
+                evento.IdEvento= Guid.NewGuid();
+                _context.Evento.Add(evento);
+
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            
+        }
+
+        public void Deletar(Guid id)
+        {
+            try
+            {
+                Evento eventoBuscado = _context.Evento.Find(id)!;
+
+                if (eventoBuscado != null)
+                {
+                    _context.Evento.Remove(eventoBuscado);
+                }
+
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            
+        }
+
+        public List<Evento> Listar()
+        {
+            try
+            {
+                return _context.Evento.Select(p => new Evento
+                {
+                    IdEvento = p.IdEvento,
+                    NomeEvento = p.NomeEvento,
+                    Descricao = p.Descricao,
+                    DataEvento = p.DataEvento,
+                    IdTipoEvento = p.IdTipoEvento,
+
+                    TiposEvento = new TiposEvento
+                    {
+                        IdTipoEvento = p.IdTipoEvento,
+                        Titulo = p.TiposEvento.Titulo
+
+                    },
+                    Instituicao = new Instituicao
+                    {
+                        IdInstituicao = p.IdInstituicao,
+                        NomeFantasia = p.Instituicao.NomeFantasia
+                    }
+
+
+                }).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public List<Evento> ListarProximos()
+        {
+            try
+            {
+                return _context.Evento
+                    .Where(e => e.DataEvento > DateTime.Now).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
